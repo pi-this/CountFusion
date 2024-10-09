1a0d3295b6ca8c0c3b6be924303e60c0154f7e3b
Pi-this <worldofwesley@att.net>
2024-10-09 15:57:09 -0400

    Settings Bar.

---
+++ Full Count/Full Count/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Full Count
+//
+//  Created by Wesley Chastain on 10/9/24.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
