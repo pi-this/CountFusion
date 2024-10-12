e97a87963faf5a71ef57649b81bf84cfaa0cee7c
Pi-this <worldofwesley@att.net>
2024-10-12 16:06:18 -0400

    Custom Count

--- Full Count/Full Count/OpCount.swift
+++ Full Count/Full Count/OpCount.swift
@@ -1,18 +1,18 @@
 //
-//  Test.swift
+//  OpCount.swift
 //  Full Count
 //
-//  Created by Wesley Chastain on 10/9/24.
+//  Created by Wesley Chastain on 10/12/24.
 //
 
 import SwiftUI
 
-struct Test: View {
+struct OpCount: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Test()
+    OpCount()
 }
---
+++ Full Count/Full Count/SimCount.swift
@@ -0,0 +1,18 @@
+//
+//  SimCount.swift
+//  Full Count
+//
+//  Created by Wesley Chastain on 10/11/24.
+//
+
+import SwiftUI
+
+struct SimCount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimCount()
+}
