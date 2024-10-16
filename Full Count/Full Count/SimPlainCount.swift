e20eebfd2dd9f0272a63380e50ae9188b30627e4
Pi-this <worldofwesley@att.net>
2024-10-16 14:23:18 -0400

    BugFix & SImple +/- counter

---
+++ Full Count/Full Count/SimPlainCount.swift
@@ -0,0 +1,18 @@
+//
+//  SimPlainCount.swift
+//  Full Count
+//
+//  Created by Wesley Chastain on 10/16/24.
+//
+
+import SwiftUI
+
+struct SimPlainCount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimPlainCount()
+}
