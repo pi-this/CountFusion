eb26811552e19d32bed109b58f3cd9f65b3a07a0
Pi-this <worldofwesley@att.net>
2024-11-02 12:21:27 -0400

    minor changes.

---
+++ Full Count/Full Count/Score Types/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  Full Count
+//
+//  Created by Wesley Chastain on 11/2/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
---
+++ Full Count/Full Count/Score Types/ToDo/SimPlainCount.swift
@@ -0,0 +1,67 @@
+//
+//  SimPlainCount.swift
+//  Full Count
+//
+//  Created by Wesley Chastain on 10/16/24.
+//
+
+import SwiftUI
+
+struct SimPlainCount: View {
+    @AppStorage("simPlainNum") var simPlainNum:Double = 0
+    @AppStorage("addAllByStr") var addAllByStr = ""
+    
+    func formatNumber(_ number: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 10
+        formatter.numberStyle = .decimal
+        return formatter.string(from: NSNumber(value: number)) ?? ""
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Spacer()
+                Button("-") {
+                    simPlainNum -= Double(addAllByStr) ?? 1
+                }
+                .buttonStyle(.bordered)
+                .foregroundColor(.red)
+                .edgesIgnoringSafeArea(.all)
+                .border(Color.red)
+                Spacer()
+                Text("\(formatNumber(simPlainNum))")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+                
+                Button("+") {
+                    simPlainNum += Double(addAllByStr) ?? 1
+                }
+                .buttonStyle(.bordered)
+                .foregroundColor(.green)
+                .edgesIgnoringSafeArea(.all)
+                .border(Color.green)
+                Spacer()
+                Spacer()
+            }
+            HStack {
+                Button("clear") {
+                    simPlainNum = 0
+                }
+                .buttonStyle(.bordered)
+                .foregroundColor(.orange)
+                .edgesIgnoringSafeArea(.all)
+                .border(Color.yellow)
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SimPlainCount()
+}
